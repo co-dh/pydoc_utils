16ffdae30c8655fd1a60792439169129d30ed074
Hao Deng <denghao8888@gmail.com>
2017-10-11 17:22:04 -0400

    finished doc.

--- counsel-pydoc.el
+++ counsel-pydoc.el
@@ -28,11 +28,13 @@
 ;; To invalidate the cache after new package installed, run counsel-pydoc with universal arguments.
 ;;
 ;; Usage:
-;;   (with virtual env)
-;;   1. pip install pydoc_utils (into a virtualenv)
-;;   2. activate your virtual environment. (pyvenv-activate recommended)
-;;   3. M-x counsel-pydoc
-;;
+;;   with virtual env:
+;;     1. pip install pydoc_utils (into a virtualenv)
+;;     2. activate your virtual environment. (pyvenv-activate recommended)
+;;     3. M-x counsel-pydoc
+;;   without virtual env:
+;;    1. sudo pip install pydoc_utils
+;;    2. M-x counsel-pydoc
 ;;; Code:
 
 (require 'swiper)
